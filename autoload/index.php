fa0e10dc0ebd6b87dc49735b990f3815bca5ab80
unknown <you@example.com>
2021-04-08 10:21:45 +0400

    file_name and

--- autoload/index.php
+++ autoload/index.php
@@ -1,9 +1,9 @@
 <?php
 spl_autoload_register(function($class_name){
-    var_dump($class_name.'.php');
+
     include $class_name.'.php';
 
 });
 
-
+new \ForBClass\B\B();
 
--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
+
 $segments = explode('/', ltrim($_SERVER['REQUEST_URI'], '/'));
 
 $controller = ucfirst($segments[0]);
+
+
+
 
 if (file_exists("Controllers/" . $controller . ".php")) {
     spl_autoload_register(function ($class_name) {
         include $class_name . '.php';
     });
-    $obj = new \Controllers\Auth();
+    $controllerClass = "Controllers\\" . $controller;
+
+
+    $obj = new $controllerClass ;
     if (method_exists($obj, $segments[1])) {
         $method = $segments[1];
         $obj->$method();
     } else {
         echo "there are not $segments[1] method";
-        exit();
+
     }
 } else {
     echo "file $controller  not exist";
-    exit();
+
 }
-
-
-
-
 
