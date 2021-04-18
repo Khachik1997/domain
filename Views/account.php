8b9259e4c09c3469a82b8ab99cf162d559314e8f
unknown <you@example.com>
2021-04-18 23:33:23 +0400

    plus friends page

--- Views/account.php
+++ Views/account.php
@@ -1,16 +1,23 @@
 
 <div class="d-flex bd-highlight mb-3" >
-    <div class="p-2 bd-highlight"><img src="../assets/images/avatar/<?= $this->userAvatar ?>" class="img-thumbnail" alt="..." style="display: inline-block;width: 160px;height: 160px;" ></div>
+    <div class="p-2 bd-highlight"><img src="/assets/images/avatar/<?= $this->userAvatar ?>" class="img-thumbnail" alt="..." style="display: inline-block;width: 160px;height: 160px;min-width: 160px" ></div>
     <div class="p-2 bd-highlight">
         <p> Name : <?= $this->userName ?></p>
-        <p> Email : <?= $this->userEmail ?></p>
+        <p style="white-space: nowrap"> Email : <?= $this->userEmail ?></p>
         <p style="color: red"><?= $this->uploadError ?></p>
     </div>
 </div>
+
+
+
+
+
+
+<?php if(!$this->friendProfile){?>
 <form action="" method="POST" class="formChangeAvatar"  enctype="multipart/form-data">
     <input type="file" name="avatar" onchange="this.form.submit()" value="Change Avatar" id="changeAvatar">
 
-    <?php if(!$this->friendProfile){?>
+
         <label for="changeAvatar">Change Avatar</label>
         <?php
     }
--- Views/friends.php
+++ Views/friends.php
@@ -1,16 +1,21 @@
 
 
+
 <?php foreach ($this->friends as $friend){
      if($friend['avatar'] == 'NULL'){$friend['avatar'] = "default.jpg";} ?>
-<div class="card" style="width:200px;">
-    <div class="card-body">
-        <h4 class="card-title">Name:<?= $friend['name'] ?></h4>
-        <p class="card-text">Email:<?= $friend['email'] ?></p>
-    </div>
 
-    <img class="card-img-bottom" src="/assets/images/avatar/<?= $friend['avatar'] ?>" alt="picture" >
-    <a href="/account/profile/<?= $friend['id'] ?>" class="btn btn-primary">See Profile</a>
-</div>
+    <div class="userCard">
+        <div class="userAvatar">
+            <img  src="/assets/images/avatar/<?= $friend['avatar'] ?>" alt="picture">
+        </div>
+        <div class="userAbout">
+            <h4 class="card-title">Name:<?= $friend['name'] ?></h4>
+            <p class="card-text">Email:<?= $friend['email'] ?></p>
+            <a href="/account/profile/<?= $friend['id'] ?>" class="btn btn-primary">See Profile</a>
+        </div>
+
+
+    </div>
     <?php
 }
 ?>
