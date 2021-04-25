eef72cc41f79f7ec73940b79d7399ace825e2410
unknown <you@example.com>
2021-04-26 01:21:24 +0400

    messagTime

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,8 +1,9 @@
-
 function scrollDown() {
     let element = document.querySelector(".messages");
     element.scrollTop = element.scrollHeight;
 }
+
+let msgDateId = 0;
 
 window.onload = scrollDown;
 
@@ -10,6 +11,16 @@ window.onload = scrollDown;
 $('#send').on('click', () => {
     let textMessage = $('#textMessage').val();
     if (textMessage.length > 0) {
+        let src = `/assets/images/avatar/${userAvatar}`;
+        let lastMessage = "<div class='container' id='lastMsg'>"
+            +
+            `<img src= ${src} alt='avatar'  />`
+            +
+            " <p>" + textMessage + "</p>" +
+            `<span class='time-left' id='${msgDateId}'>` + " </span>" +
+            "</div>";
+        $('.messages').append(lastMessage);
+        scrollDown();
         let x = $.ajax({
             url: `/account/sendMessage/${friendId}`,
             type: 'POST',
@@ -17,25 +28,15 @@ $('#send').on('click', () => {
             dataType: "json",
             success: function printMsg(response) {
                 if (response["success"]) {
-                    let src = `/assets/images/avatar/${userAvatar}`;
-                    let lastMessage = "<div class='container ' id='lastMsg'>"
-                        +
-                        `<img src= ${src} alt='avatar' />`
-                        +
-                        " <p>" + textMessage + "</p>" +
-                        "<span class='time-right'>" + response["date"] + " </span>" +
-                        "</div>";
-                    lastMsgId++;
-                    $('.messages').append(lastMessage);
-                    scrollDown()
+                    let elementForTime = document.getElementById(`${msgDateId}`)
+                    elementForTime.innerText = response['date'];
                 }
-
-
+                msgDateId++;
+                lastMsgId++;
             }
         });
     }
     $('#textMessage').val("");
-
 })
 
 
@@ -49,17 +50,17 @@ setInterval(() => {
             if (response.length > 0) {
                 lastMsgId = response[response.length - 1]['id'];
                 response.forEach((response) => {
-                    if(response["from_id"] !== sessionId ){
+                    if (response["from_id"] !== sessionId) {
                         if (!response['avatar']) {
                             response['avatar'] = "default.jpg";
                         }
                         let src = `/assets/images/avatar/${response['avatar']}`;
                         let lastMessage = "<div class='container darker''>"
                             +
-                            `<img src= ${src} alt='avatar' />`
+                            `<img src= ${src} alt='avatar' class='right'/>`
                             +
-                            " <p>" + response['body'] + "</p>" +
-                            "<span class='time-left'>" + response['date'] + " </span>" +
+                            " <p class='right'>" + response['body'] + "</p>" +
+                            "<span class='time-right'>" + response['date'] + " </span>" +
                             "</div>";
                         $('.messages').append(lastMessage);
                         scrollDown();
